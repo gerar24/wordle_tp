d5f52317f40428283ccf74d158e86121209e765d
gerar24 <gerardonahuel@hotmail.com>
2024-12-03 20:53:30 -0300

    Add alerta caracter invalido + embellecido de CLI.hs (comentado + indentado)

--- src/CLI.hs
+++ src/CLI.hs
@@ -6,7 +6,7 @@ import System.Environment (getArgs)
 import System.IO.Error
 import System.Random.Stateful (uniformRM, globalStdGen)
 import TinyApp.Interactive
-import Data.Char (toUpper, toLower)
+import Data.Char (toUpper, toLower, isLetter)
 import Data.Yaml (decodeFileEither)
 import Data.Maybe (isJust)
 import qualified Data.Set as Set
@@ -33,42 +33,46 @@ main = do
                                 putStrLn "The word is not in the dictionary. Please try again."
                                 --runInteractive' (wordle (Estado {juego = nuevo "hello" 5, intentoActual = [], alertas = []}))
                 Nothing -> do
-                    putStrLn "Let's start the game with random word."
-                    rWord <- selectRandomWord diccInicial
-                    let randomWord = map toLower rWord
-                    runInteractive (wordle (Estado {juego = nuevo randomWord 5, intentoActual = [], alertas = [], diccionario=diccInicial, descartadas=Set.empty}))
- -- SI NOTHING DAILY
- -- SI DAILY DAILY
- -- SI DAILY + FECHA, DAILY FECHA
- -- SI PALABRA PALABRA
-
+                                putStrLn "Let's start the game with random word."
+                                rWord <- selectRandomWord diccInicial
+                                let randomWord = map toLower rWord
+                                runInteractive (wordle (Estado {juego = nuevo randomWord 5, intentoActual = [], alertas = [], diccionario=diccInicial, descartadas=Set.empty}))
     pure()
 
+-- Coments para W08
+                    -- SI NOTHING DAILY
+                    -- SI DAILY DAILY
+                    -- SI DAILY + FECHA, DAILY FECHA
+                    -- SI PALABRA PALABRA
+
+--- Seleciona palabra random
 selectRandomWord :: Diccionario -> IO String
 selectRandomWord dicc = do
-    let listaPalabras = vocabulario dicc
-    indice <- uniformRM (0, length listaPalabras - 1) globalStdGen
-    return (listaPalabras !! indice)
+                            let listaPalabras = vocabulario dicc
+                            indice <- uniformRM (0, length listaPalabras - 1) globalStdGen
+                            return (listaPalabras !! indice)
 
-    
-
+--- Carga diccionario
 loadDictionary :: FilePath -> IO (Maybe [String])
 loadDictionary path = do
-    result <- decodeFileEither path
-    case result of
-        Right diccionario -> return (Just diccionario)
-        Left _ -> do
-            putStrLn "Error al cargar el diccionario."
-            return Nothing
+                        result <- decodeFileEither path
+                        case result of
+                                        Right diccionario -> return (Just diccionario)
+                                        Left _ -> do
+                                                    putStrLn "Error al cargar el diccionario."
+                                                    return Nothing
 
+--- Parsea args para modo de run
 parseArgs :: [String] -> Maybe String
 parseArgs ("--palabra":valor:_) = Just valor
 parseArgs _ = Nothing
 
+--- Tipo diccionario
 newtype Diccionario = Diccionario {
     vocabulario :: [String]
 }
 
+--- Clase de Estado
 data Estado = Estado {
     juego :: Juego,
     intentoActual :: String,
@@ -77,16 +81,19 @@ data Estado = Estado {
     descartadas :: Set.Set Char
 }
 
+--- Limpieza de Estado (intento en vivo y alertas que se le muestran al usuario)
 cleanState :: Estado -> Estado
 cleanState estado = estado {intentoActual=[]}
 
 cleanAlertas :: Estado -> Estado
 cleanAlertas estado = estado {alertas=[]}
 
+--- CLI Interactive principal
 wordle :: Estado -> Sandbox Estado
 wordle estadoInicial =
     Sandbox {
         initialize = estadoInicial,
+        --- Dibujamos ;)
         render = \s ->
             "\n\n\n"
             <> replicate (4 * length (palSecreta (juego s))) '-' ++ "\n"
@@ -100,28 +107,39 @@ wordle estadoInicial =
             <> "\n\n"
             <> showAlertas (alertas s)
             <> "\n\n\n\n",
-            
+
+        --- Refrescamos todo el rato...    
         update = \(Key key _) s ->
             case termino (juego s) of
                 Gano -> do
+                    --- Solo puedo salir
                     if key == KEsc
                         then
-                            (cleanState s, Exit)
-                    else
-                        (s {alertas= (alertas s) ++ ["Ganaste crack mundial ;)\nApretá 'Esc' para Salir"]}, Continue)
-                Perdio -> do
-                    if key == KEsc
-                        then
-                            (cleanState s, Exit)
+                            (s, Exit)
                         else
-                            (s {alertas= (alertas s) ++ ["Perdiste, lo siento mucho :/\nLa palabra era: " ++ map toUpper (palSecreta(juego s)) ++ "\nApretá 'Esc' para Salir"]}, Continue)
-                Continua -> case key of
+                            ((cleanAlertas s) {alertas= (alertas (cleanAlertas s)) ++ ["Ganaste crack mundial ;)\nApretá 'Esc' para Salir"]}, Continue)
+                Perdio -> do
+                            --- Solo puedo salir
+                            if key == KEsc
+                                then
+                                    (s, Exit)
+                                else
+                                    ((cleanAlertas s) {alertas= (alertas (cleanAlertas s)) ++ ["Perdiste, lo siento mucho :/\nLa palabra era: " ++ map toUpper (palSecreta(juego s)) ++ "\nApretá 'Esc' para Salir"]}, Continue)
+                Continua -> case key of 
+                            --- Puedo salir (Esc)
+                            --- Intentar Enviar Intento (Enter)
+                            --- Borrar Letra (KBS)
+                            --- Escribir Letra (KChar)
+                            --- Cualquier otra cosa (_ Continua como si nada el juego)
                                 KEsc -> (cleanState s, Exit)
                                 KEnter -> 
+                                    --- Chequeo validez de longitud
                                     if longPalSecreta (juego s) == length (intentoActual s)
                                         then
+                                            --- Chequeo validez de palabra en diccionario
                                             if map toUpper (intentoActual s) `elem` vocabulario (diccionario s)
                                                 then
+                                                    --- Chequeo si el juego lo acepta (quedó medio redundante dado ciclo de juego que evita jugar si ya terminó)
                                                     case enviarIntento (intentoActual s) (juego s) of
                                                         Just nuevoJuego -> 
                                                             let estadoActualizado = s { juego = nuevoJuego, 
@@ -131,49 +149,59 @@ wordle estadoInicial =
                                                 else
                                                     (s {alertas = (alertas s) ++ ["Esa palabra no es válida."]}, Continue)
                                         else
-                                            (s {alertas = (alertas s) ++ ["Longitud inválida."]}, Continue)--(cleanState s, Continue)
+                                            (s {alertas = (alertas s) ++ ["Longitud inválida."]}, Continue)
 
-                                KBS -> if length (intentoActual s) > 0
+                                KBS -> if length (intentoActual s) > 0 --- Puedo borrar solo si hay letras por borrar
                                             then
                                                 let estadoActualizado = s {intentoActual = init (intentoActual s)}
                                                 in (cleanAlertas estadoActualizado, Continue)
                                             else
                                                 (s,Continue)
 
-                                KChar pressed -> if longPalSecreta (juego s)  > length (intentoActual s)
+                                KChar pressed -> if isLetter pressed --- Chequeo validez de input.
                                                     then
-                                                        if Set.member pressed (descartadas s)
+                                                        --- Chequeo no pasarme de la "grilla"
+                                                        if longPalSecreta (juego s)  > length (intentoActual s)
                                                             then
-                                                                let estadoActualizado = s {intentoActual = intentoActual s ++ [toLower pressed], alertas = (alertas s) ++ ["Atención! La letra " ++ [toUpper pressed] ++ " ya la descartaste antes..."]}
-                                                                in (estadoActualizado, Continue)
+                                                                --- Chequeo si ya la descarté para dar alerta
+                                                                if Set.member pressed (descartadas s)
+                                                                    then
+                                                                        let estadoActualizado = s {intentoActual = intentoActual s ++ [toLower pressed], alertas = (alertas s) ++ ["Atención! La letra " ++ [toUpper pressed] ++ " ya la descartaste antes..."]}
+                                                                        in (estadoActualizado, Continue)
+                                                                    else
+                                                                        let estadoActualizado = s {intentoActual = intentoActual s ++ [toLower pressed]}
+                                                                        in (estadoActualizado, Continue)
                                                             else
-                                                                let estadoActualizado = s {intentoActual = intentoActual s ++ [toLower pressed]}
-                                                                in (estadoActualizado, Continue)
+                                                                (s, Continue)
                                                     else
-                                                        (s, Continue)
+                                                        (s {alertas = (alertas s) ++ ["Carácter inválido."]}, Continue) -- Curiosidad, pensé que cuando "carácter" se refería a letra iba sin tilde, pero no es así
+                                _ -> (s, Continue) --- Casos que "no nos importan"
                                 
     }
 
+--- Mostrar alertas en líneas separadas
 showAlertas :: [String] -> String
 showAlertas alertas = concatMap (++ "\n") alertas
 
+--- Mostrar letras ya descartadas
 showYaDescartadas :: Set.Set Char -> String
 showYaDescartadas descartadas =
-    let lista = Set.toList descartadas
-    in unwords (map (\c-> [toUpper c]) lista)
+                                let lista = Set.toList descartadas
+                                in unwords (map (\c-> [toUpper c]) lista)
 
-
+--- Calcular conjunto de letras que ya descarto el jugador
 yaDescartadas :: [(Char, Match)] -> Set.Set Char
 yaDescartadas [] = Set.empty
 yaDescartadas ((char , match):xs)
     | match == NoPertenece = Set.insert char (yaDescartadas xs)
     | otherwise = yaDescartadas xs
 
+--- Pretty prints del juego ;)
 showJuego :: Juego -> String
 showJuego (Juego psec _ ints) =
         case ints of
-            [] -> ""
-            _ -> concatMap showIntentoPasado ints
+                    [] -> ""
+                    _ -> concatMap showIntentoPasado ints
 
 showIntentoPasado :: [(Char, Match)] -> String
 showIntentoPasado [] = ""
